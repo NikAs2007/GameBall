73c09f35cbba07090f0b5aa7a913db4174a2adc7
SashaWindows <nikonorovsasha1@gmail.com>
2026-04-22 23:43:07 +0300

    Начинаю добавлять онлайн

---
+++ GameBallLib/header.h
@@ -0,0 +1,133 @@
+#pragma once
+
+#define M_PI 3.141592
+#define ASIO_STANDALONE
+
+#include <asio.hpp>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <cmath>
+#include <random>
+#include <chrono>
+using namespace std;
+using namespace sf;
+
+extern Keyboard keyboard;
+extern Mouse mouse;
+extern const int sizeofscreenx;
+extern const int sizeofscreeny;
+//extern float k_size;
+//extern Vector2f corKam;
+
+int sign(float a);
+Vector2f toOneVec(Vector2f vec);
+float mod(Vector2f vec);
+float hiperbola(float x);
+Vector2f rotateVector(Vector2f vec, float angle);
+
+struct Body {
+	CircleShape body;
+	float rad = 10;
+	Vector2f cor;
+
+	//void setRad(float rad);
+	//CircleShape& getBody() {
+	//	return body;
+	//}
+	virtual void polimorphicCl() {};
+};
+
+class Player : public Body{
+	//CircleShape body;
+	Vector2f acs;
+	Vector2f vel;
+	float mass = 500;
+	float power = 2;
+	float kfr = 0.3;
+	float kai = 1.5;
+
+	Text dtext;
+	static Font font;
+
+	friend class Enemy;
+
+public:
+	Player();
+	Text getDebug();
+	Vector2f calc_acs(Vector2f vec);
+	void control();
+	CircleShape getBody();
+};
+
+class Enemy : public Body {
+	Vector2f acs;
+	Vector2f vel;
+	float mass = 500;
+	float power = 3;
+	float kfr = 0.05;
+	float kai = 2;
+
+	Player* target;
+public:
+	Enemy(Player* target);
+	Vector2f calc_acs(Vector2f vec);
+	void control();
+	CircleShape getBody();
+};
+
+class Button : public Body {
+
+public:
+	bool isHover(RenderWindow& window) {
+		if (mod(Vector2f(mouse.getPosition(window)) - body.getPosition()) <= rad) {
+			rad = 55;
+			body.setRadius(rad);
+			body.setFillColor(Color(200,245,255));
+			return true;
+		}
+		rad = 50;
+		body.setRadius(rad);
+		body.setFillColor(Color(255,255,255));
+		return false;
+	}
+
+	Button() {
+		rad = 50;
+		cor = Vector2f(0, 0);
+		body.setFillColor(Color(255, 255, 255, 255));
+		body.setOrigin(rad, rad);
+		body.setPointCount(64);
+		body.setRadius(rad);
+	}
+};
+
+class Menu {
+
+public:
+	vector<Button> buttons;
+	enum Stage { MENU, PAUSE, PLAY };
+	Stage stage = MENU;
+
+	Menu();
+	
+};
+
+class Camera {
+	vector<Body*>* bodies;
+	Menu* menu;
+	RenderWindow* window;
+	float k_size = 1;
+	Vector2f corKam = Vector2f(0, 0);
+
+	Shader blinks;
+	Shader screen_sh;
+	Clock clock;
+
+	RenderTexture rendTexture;
+	Sprite picture;
+
+public:
+	Camera(RenderWindow* window, vector<Body*>* bodies, Menu* menu);
+	void control();
+	void draw_all();
+};
---
+++ client/main.cpp
@@ -0,0 +1,135 @@
+//client
+
+#include "../GameBallLib/header.h"
+
+const int sizeofscreenx = 1800;
+const int sizeofscreeny = 700;
+//float k_size = 1;
+//Vector2f corKam = Vector2f(sizeofscreenx, sizeofscreeny);
+
+int main()
+{
+    srand(static_cast<unsigned int>(chrono::system_clock::now().time_since_epoch().count()));
+    sf::Clock clock;
+    sf::ContextSettings settings;
+    Player player;
+    vector<Enemy> enemy;
+
+    for (int i = 0; i < 50; ++i) enemy.push_back(Enemy(&player));
+
+    vector<Body*> bodies;
+    bodies.push_back(&player);
+    for (int i = 0; i < 50; ++i) bodies.push_back(&enemy[i]);
+
+
+    settings.antialiasingLevel = 5;
+    sf::RenderWindow window(sf::VideoMode(sizeofscreenx, sizeofscreeny), "Game Ball", sf::Style::Default, settings);
+    Menu menu;
+    Camera cam(&window, &bodies, &menu);
+    bool fl = false;
+    bool fl_pause = false;
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+        }
+
+
+
+        //if (menu.stage != menu.MENU) {
+        //    if (fl_pause) {
+        //        if (!keyboard.isKeyPressed(keyboard.P)) {
+        //            fl_pause = false;
+        //        }
+
+
+        //    }
+        //    else {
+        //        if (keyboard.isKeyPressed(keyboard.P)) {
+        //            if (menu.stage == menu.PLAY) menu.stage = menu.PAUSE;
+        //            else if (menu.stage == menu.PAUSE) menu.stage = menu.PLAY;
+        //            fl = true;
+        //        }
+        //    }
+        //}
+
+
+        if (menu.stage == menu.PLAY) {
+            player.control();
+            for (int i = 0; i < enemy.size(); ++i) {
+                enemy[i].control();
+            }
+        }
+        else if (menu.stage == menu.MENU) {
+            for (int i = 0; i < menu.buttons.size(); ++i) {
+                menu.buttons[i].isHover(window);
+                if (fl) {
+                    if (!menu.buttons[i].isHover(window) && !mouse.isButtonPressed(mouse.Left)) {
+                        fl = false;
+                    }
+                    else if (!mouse.isButtonPressed(mouse.Left) && menu.buttons[i].isHover(window)) {
+                        menu.stage = menu.PLAY;
+                        fl = false;
+                    }
+
+                }
+                else {
+                    if (mouse.isButtonPressed(mouse.Left) && menu.buttons[i].isHover(window)) {
+                        fl = true;
+                    }
+                }
+
+            }
+        }
+
+
+        cam.control();
+        cam.draw_all();
+
+    }
+
+    return 0;
+}
+
+int sign(float a) {
+    if (a >= 0) return 1;
+    else return -1;
+}
+
+Vector2f toOneVec(Vector2f vec) {
+    if (pow(vec.x * vec.x + vec.y * vec.y, 0.5) == 0) return Vector2f(0, 0);
+    Vector2f newVec(vec.x, vec.y);
+    newVec.x /= pow(vec.x * vec.x + vec.y * vec.y, 0.5);
+    newVec.y /= pow(vec.x * vec.x + vec.y * vec.y, 0.5);
+    return newVec;
+}
+
+float mod(Vector2f vec) {
+    return pow(vec.x * vec.x + vec.y * vec.y, 0.5);
+}
+
+float hiperbola(float x) {
+    if (x <= 0.001) return 1000;
+    return x;
+}
+
+Vector2f rotateVector(Vector2f vec, float ang) {
+    float angle = abs(ang) * M_PI / 180.0f;
+    float cos_a = std::cos(angle);
+    float sin_a = std::sin(angle);
+
+    if (ang < 0) {
+        sin_a = -sin_a;  // меняем знак синуса для поворота в другую сторону
+    }
+
+    Vector2f ans = Vector2f(
+        vec.x * cos_a - vec.y * sin_a,
+        vec.x * sin_a + vec.y * cos_a
+    );
+    return ans;
+}
